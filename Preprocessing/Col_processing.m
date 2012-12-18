d3dcc4671be8d193dd5e2e54b19d67e4ed256889
unknown <honestboye@yahoo.cn>
2012-12-18 23:22:13 +0800

    new version

--- Preprocessing/Col_processing.m
+++ Preprocessing/Col_processing.m
@@ -3,21 +3,22 @@ function [ Cleaned ] = Col_processing( Raw )
 %   Raw: a vector hold the data in one colunm sequently
 %       it has three fields: title,type and data, among which are cells
 %   using the formula: x'=(x-min)/(max-min)
-
+    assert(isstruct(Raw),'Input data is not a struct');
 %   Standarlize the data
-    Cleaned = zeros(length(Raw.data));
-    if Raw.type == 'D'
+    Cleaned = zeros(length(Raw.data),1);
+    ExistedValue = 0;
+    if Raw.type{1} == 'D'
         for i = 1 : length(Raw.data)
             index = Index(ExistedValue,Raw.data(i));
             if  index == 0
                 ExistedValue(length(ExistedValue)+1) = Raw.data(i);
             end
-            Cleaned(i) = index; 
+            Cleaned(i) = index;  
         end
-    else if Raw.type == 'C'
+    else if Raw.type{1} == 'C'
         Cleaned = (Raw.data - min(Raw.data)) * (max(Raw.data) - min(Raw.data))^(-1);
         % imagine we have 5 intevals to discretize the continuous data.
-        Discretize(Cleaned,5)
+        Discretize(Cleaned,30)
         end
     end
 end
--- Preprocessing/Preprocess.m
+++ Preprocessing/Preprocess.m
@@ -2,15 +2,16 @@ function [ Matrix ] = Preprocess( RawCells )
 %PREPROCESS preprocess the data 
 %   RawCells: with 3 fields, including title, type and data, which are all
 %   strings
-
-%   Remove the rows with NULL cells
-%...
+    assert(isstruct(RawCells),'Input is not struct');
     
     Matrix.title = RawCells.title;
     Matrix.type = RawCells.type;
     Matrix.data = zeros(length(RawCells.data(:,1)),length(RawCells.data(1,:)));
     for i = 1:length(RawCells.title)
-        Matrix.data(:,i) = Col_processing(RawCells.data(:,i));
+        Col.title=Matrix.title(i);
+        Col.type=Matrix.type(i);
+        Col.data=RawCells.data(:,i);
+        Matrix.data(:,i) = Col_processing(Col);
     end
 
 end
