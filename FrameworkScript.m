da045ad5fcdbc94c8158cd1a87843fcdda8ede60
Doris Yang <doris.yx@gmail.com>
2012-12-24 01:42:39 +0800

    complex T_T

--- FrameworkScript.m
+++ FrameworkScript.m
@@ -1,9 +1,14 @@
 data = readData('ISBSG[USE].xls');
 data = excludeNaN(data);
 data = encapsulateData(data);
- 
-Cleaned = Preprocess(data);
-csvwrite('Cleaned(1).csv',Cleaned.data);
 
-FSRlt = MIFS(Cleaned,9,1);
-csvwrite('FS.csv',FSRlt.data);
+Cleaned = Preprocess(data);
+
+CrossValidation(Cleaned);
+
+%Cleaned = Preprocess(data);
+%csvwrite('Cleaned(1).csv',Cleaned.data);
+
+%FSRlt = MIFS(Cleaned,9,1);
+%csvwrite('FS.csv',FSRlt.data);
+
