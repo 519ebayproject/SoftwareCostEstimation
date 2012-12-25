49e84f0179fcd799f59d8704803fa6515b0167ee
Doris Yang <doris.yx@gmail.com>
2012-12-25 18:20:36 +0800

    cross validation almost done

--- CrossValidation/ValidationFramework.m
+++ CrossValidation/ValidationFramework.m
@@ -1,4 +1,5 @@
-data = readData('ISBSG[USE].xls');
+function [args] = ValidationFramework(filepath,minK,maxK,minH,maxH,incH)
+data = readData(filepath);
 data = excludeNaN(data);
 data = encapsulateData(data);
 
@@ -12,7 +13,7 @@ cleaned = Preprocess(data);
 featureSets=[];
 
 for i=1:length(trainSets);
-   fsRlt = MIFS(trainSets(i),9,1);
+   fsRlt = MIFS(trainSets(i),5,1);
    featureSets=[featureSets;fsRlt];
 end
 
@@ -20,36 +21,140 @@ end
 
  % just for one kind method first
  
+ fprintf('for knn k from %d to %d:\n',minK,maxK);
+  output = [];
+ for knnInc = minK:maxK
  % validation begins
- results=[];
-for testSetIdx=1:length(testSets)
-    currentTestSetData = testSets(testSetIdx).data;
-    currentTrainSetData = featureSets(testSetIdx).data;
-    
-    [testSetRol,testSetCol]=size(currentTestSetData);
-    
-    for testCaseIdx=1:testSetRol
-        % do case selection
-       % neighbours = tj_knn(currentTrainSetData,3,currentTestSetData(testCaseIdx,:));
-       neighbours = tj_parzen_window(currentTrainSetData,4.0,currentTestSetData(testCaseIdx,:));
+ resultsMean=[];
+ resultsWeighted=[];
+    for testSetIdx=1:length(testSets)
+        currentTestSetData = testSets(testSetIdx).data;
+        currentHistorySetData = featureSets(testSetIdx).data;
 
-        % do case adaption
-        est = tj_CaseAdaption_Mean(neighbours);
+        [testSetRow,testSetCol]=size(currentTestSetData);
+
+        for testCaseIdx=1:testSetRow
+            % do case selection
+           neighbours = tj_knn(currentHistorySetData,knnInc,currentTestSetData(testCaseIdx,:));
+           
+            % do case adaption
+            estMean = tj_CaseAdaption_Mean(neighbours);
+            estWeighted = tj_CaseAdaption_Weighted(neighbours);
+
+            %find the acutal effort from the input test set
+            act = currentHistorySetData(testCaseIdx,testSetCol);
+
+            resultsMean=[resultsMean;[act,estMean]];
+            resultsWeighted = [resultsWeighted;[act,estWeighted]];
+        end    
+    end
+           % disp(results);
+
+        %for the results, calculate related metrics
+        mmreMean = MMRE(resultsMean(:,1),resultsMean(:,2));
+        mdmreMean = MdMRE(resultsMean(:,1),resultsMean(:,2));
+        predMean = PRED025(resultsMean(:,1),resultsMean(:,2));
         
-        %find the acutal effort from the input test set
-        act = currentTrainSetData(testCaseIdx,testSetCol);
+        mmreWeighted = MMRE(resultsWeighted(:,1),resultsWeighted(:,2));
+        mdmreWeighted = MdMRE(resultsWeighted(:,1),resultsWeighted(:,2));
+        predWeighted = PRED025(resultsWeighted(:,1),resultsWeighted(:,2));
         
-        results=[results;[act,est]];
-    end    
-    disp(results);
-    
-    %for the results, calculate related metrics
-    mmre = MMRE(results(:,1),results(:,2));
-    mdmre = MdMRE(results(:,1),results(:,2));
-    pred = PRED025(results(:,1),results(:,2));
-    
-    disp(mmre);
-    disp(mdmre);
-    disp(pred);
-    
+        output = [output;[knnInc,mmreMean,mdmreMean,predMean]];
+        output = [output;[knnInc,mmreWeighted,mdmreWeighted,predWeighted]];
+        
+ end
+       % output the results from experiments of KNN
+       csvwrite('knn result.csv',output);
+ 
+ 
+ fprintf('for parzen window h from %d to %d increased by %d:\n',minH,maxH,incH);
+ output = [];
+ for wndInc = minH:incH:maxH
+     resultsMean=[];
+     resultsWeighted=[];
+    for testSetIdx=1:length(testSets)
+        currentTestSetData = testSets(testSetIdx).data;
+        currentHistorySetData = featureSets(testSetIdx).data;
+
+        [testSetRow,testSetCol]=size(currentTestSetData);
+
+        for testCaseIdx=1:testSetRow
+            % do case selection
+          % neighbours = tj_knn(currentHistorySetData,knnInc,currentTestSetData(testCaseIdx,:));
+           neighbours = tj_parzen_window(currentHistorySetData,wndInc,currentTestSetData(testCaseIdx,:));
+
+            % do case adaption
+            estMean = tj_CaseAdaption_Mean(neighbours);
+            estWeighted = tj_CaseAdaption_Weighted(neighbours);
+
+            %find the acutal effort from the input test set
+            act = currentHistorySetData(testCaseIdx,testSetCol);
+
+            resultsMean=[resultsMean;[act,estMean]];
+            resultsWeighted = [resultsWeighted;[act,estWeighted]];
+        end    
+    end
+           % disp(results);
+
+        %for the results, calculate related metrics
+        mmreMean = MMRE(resultsMean(:,1),resultsMean(:,2));
+        mdmreMean = MdMRE(resultsMean(:,1),resultsMean(:,2));
+        predMean = PRED025(resultsMean(:,1),resultsMean(:,2));
+        
+        mmreWeighted = MMRE(resultsWeighted(:,1),resultsWeighted(:,2));
+        mdmreWeighted = MdMRE(resultsWeighted(:,1),resultsWeighted(:,2));
+        predWeighted = PRED025(resultsWeighted(:,1),resultsWeighted(:,2));
+        
+        output = [output;[wndInc,mmreMean,mdmreMean,predMean]];
+        output = [output;[wndInc,mmreWeighted,mdmreWeighted,predWeighted]];      
+ end
+    % output the results from experiments of Parzen Window
+       csvwrite('parzen window result.csv',output);
+ 
+ 
+  disp('for mkp:\n');
+  output =[];
+ for wndInc = minH:incH:maxH
+ for knnInc= minK:maxK
+     resultsMean=[];
+    for testSetIdx=1:length(testSets)
+        currentTestSetData = testSets(testSetIdx).data;
+        currentHistorySetData = featureSets(testSetIdx).data;
+
+        [testSetRow,testSetCol]=size(currentTestSetData);
+
+        for testCaseIdx=1:testSetRow
+            % do case selection
+          % neighbours = tj_knn(currentHistorySetData,knnInc,currentTestSetData(testCaseIdx,:));
+           neighbours = tj_mkp(currentHistorySetData,knnInc,wndInc,currentTestSetData(testCaseIdx,:));
+
+             % do case adaption
+            estMean = tj_CaseAdaption_Mean(neighbours);
+            estWeighted = tj_CaseAdaption_Weighted(neighbours);
+
+            %find the acutal effort from the input test set
+            act = currentHistorySetData(testCaseIdx,testSetCol);
+
+            resultsMean=[resultsMean;[act,estMean]];
+            resultsWeighted = [resultsWeighted;[act,estMean]];
+        end    
+    end
+           % disp(results);
+
+        %for the results, calculate related metrics
+        mmreMean = MMRE(resultsMean(:,1),resultsMean(:,2));
+        mdmreMean = MdMRE(resultsMean(:,1),resultsMean(:,2));
+        predMean = PRED025(resultsMean(:,1),resultsMean(:,2));
+        
+        mmreWeighted = MMRE(resultsWeighted(:,1),resultsWeighted(:,2));
+        mdmreWeighted = MdMRE(resultsWeighted(:,1),resultsWeighted(:,2));
+        predWeighted = PRED025(resultsWeighted(:,1),resultsWeighted(:,2));
+        
+        output=[output;[wndInc,knnInc,mmreMean,mdmreMean,predMean]];
+        output=[output;[wndInc,knnInc,mmreWeighted,mdmreWeighted,predWeighted]];
+ end
+        %output the results from experiments of mpk
+        csvwrite('mkp result.csv',output);
+ end
+ 
 end
--- FrameworkScript.m
+++ FrameworkScript.m
@@ -1,14 +1,16 @@
-data = readData('ISBSG[USE].xls');
-data = excludeNaN(data);
-data = encapsulateData(data);
+%data = readData('ISBSG[USE].xls');
+%data = excludeNaN(data);
+%data = encapsulateData(data);
 
-Cleaned = Preprocess(data);
+%Cleaned = Preprocess(data);
 
-CrossValidation(Cleaned);
+%CrossValidation(Cleaned);
 
 %Cleaned = Preprocess(data);
 %csvwrite('Cleaned(1).csv',Cleaned.data);
 
 %FSRlt = MIFS(Cleaned,9,1);
 %csvwrite('FS.csv',FSRlt.data);
+
+ValidationFramework('Desharnais[USE].xls',1,17,0.8,2.0,0.1);
 
